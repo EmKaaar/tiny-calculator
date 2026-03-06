3d134ddbe852ddd5ee226bfcc8f852405c0e5c1c
Karic Emin <E.Karic@htlvb.at>
2026-03-06 11:19:05 +0100

    Added first simple calculation test to program.cs

--- TinyCalculator/Program.cs
+++ TinyCalculator/Program.cs
@@ -6,7 +6,10 @@
         {
             Console.WriteLine("..: Tiny Calculator :..");
 
-            
+            int a = 5;
+            int b = 10;
+
+            Console.WriteLine($"{a} + {b} = {a+b}");
         }
     }
 }
