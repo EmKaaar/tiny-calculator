322532e31fd937d3a2fa9ebe02a1509bff6c71f5
Karic Emin <E.Karic@htlvb.at>
2026-03-06 11:07:34 +0100

    Added Subtitle to Program.cs File

--- TinyCalculator/Program.cs
+++ TinyCalculator/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Git!");
+            Console.WriteLine("..: Tiny Calculator :..");
+
             
         }
     }
