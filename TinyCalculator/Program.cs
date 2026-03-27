cba42b74359b58999cbbb461b492f44d0e16a8bd
Karic Emin <E.Karic@htlvb.at>
2026-03-27 10:11:07 +0100

    First Version of Simple Calculator

--- TinyCalculator/Program.cs
+++ TinyCalculator/Program.cs
@@ -1,15 +1,27 @@
-﻿namespace TinyCalculator
+﻿Console.WriteLine("== Tiny Calculator ==");
+
+int a, b;
+
+
+a = ReadOperand("Erster Operand");
+b = ReadOperand("Zweiter Operand");
+
+Console.ResetColor();
+
+Console.WriteLine($"{a} + {b} = {a + b}");
+Console.WriteLine($"{a} - {b} = {a - b}");
+Console.WriteLine($"{a} * {b} = {a * b}");
+if (b == 0) Console.WriteLine($"{a}/{b} ist undefiniert");
+else Console.WriteLine($"{a} / {b} = {(double)a / b}");
+
+static int ReadOperand(string title)
 {
-    internal class Program
+    Console.Write($"{title}: ");
+    int a;
+    while (!int.TryParse(Console.ReadLine(), out a))
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("..: Tiny Calculator :..");
-
-            int a = 5;
-            int b = 10;
-
-            Console.WriteLine($"{a} + {b} = {a+b}");
-        }
+        Console.WriteLine($"Fehler, Eingabe ist keine Zahl");
+        Console.Write($"{title}: ");
     }
+    return a;
 }
